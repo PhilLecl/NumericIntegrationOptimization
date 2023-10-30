692e7c11095631e9daee634f16bff6b6a81a824d
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-10-30 15:19:55 +0100

    Base setup for tests

---
+++ src/int_num.py
@@ -0,0 +1,38 @@
+import math
+
+
+def _print_nonconvergence_warning():
+    print('----------------------------------')
+    print('   WARNING: maxiter was reached   ')
+    print('Your calculation did not converge!')
+    print('----------------------------------')
+    print()
+
+
+def int_num(f, a, b, tol=1e-4, maxiter=1000):
+    _integrate = _rechteck
+    if b < a:
+        return -_integrate(f, b, a, tol, maxiter)
+    else:
+        return _integrate(f, a, b, tol, maxiter)
+
+
+def _rechteck(f, a, b, tol, maxiter):
+    segcount = 4
+    area = _rechtecksformel(f, a, b, segcount - 1)
+    for iteration in range(maxiter):
+        old_area = area
+        segcount *= 2
+        area = _rechtecksformel(f, a, b, segcount - 1)
+        if abs(area - old_area) <= tol:
+            break
+    if iteration == maxiter - 1:
+        _print_nonconvergence_warning()
+    return area
+
+
+def _rechtecksformel(f, a, b, segcount):
+    segwidth = (b - a) / segcount
+    xs = [a + (i + 0.5) * segwidth for i in range(segcount)]
+    area = sum(f(xs[i]) for i in range(segcount)) * segwidth
+    return area
---
+++ tests/functions.py
@@ -0,0 +1,13 @@
+from scipy import special
+import math
+import numpy as np
+
+def psi_harm(x, n=5):
+    H = special.hermite(n)
+    C = (1 / (np.pi)) ** (0.25) * 1 / (2 ** n * math.factorial(n)) ** (0.5)
+    res = C * H(x) * np.exp(-x ** 2 / 2)
+    return res
+
+
+def psi_harm_sq(x, n=5):
+    return psi_harm(x, n) ** 2
