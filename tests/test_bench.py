47057d7443c572034fc7d72aaa64624f6f0b4e16
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-10-30 15:50:13 +0100

    Update tests

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,21 +1,24 @@
 import pytest
-import scipy.integrate as integrate
+import math
 from src.int_num import int_num
-from tests.functions import psi_harm, psi_harm_sq
+from tests.functions import psi_harm_sq
 
 
-@pytest.mark.parametrize(('f', 'a', 'b', 'tol', 'maxiter'), [
-    (psi_harm, -5, 5, 1e-8, 1000),
-    (psi_harm, 5, -5, 1e-8, 1000),
-    (psi_harm_sq, -1, 1, 1e-8, 1000),
-    (psi_harm_sq, -2, 2, 1e-8, 1000),
-    (psi_harm_sq, -5, 5, 1e-4, 1000),
-    (psi_harm_sq, -5, 5, 1e-6, 1000),
-    (psi_harm_sq, -5, 5, 1e-8, 1000),
-    (psi_harm_sq, -5, 5, 1e-10, 1000),
-    (psi_harm_sq, -10, 10, 1e-8, 1000)
-])
-def test_int_num(benchmark, f, a, b, tol, maxiter):
-    my_result = benchmark(int_num, f, a, b, tol, maxiter)
-    scipy_result = integrate.quad(f, a, b)
-    assert abs(my_result - scipy_result[0]) <= tol
+def test_phs_rough(benchmark):
+    benchmark(int_num, psi_harm_sq, -5, 5, 1e-4, 1000)
+
+
+def test_phs(benchmark):
+    benchmark(int_num, psi_harm_sq, -5, 5, 1e-8, 1000)
+
+
+def test_phs_precise(benchmark):
+    benchmark(int_num, psi_harm_sq, -5, 5, 1e-10, 1000)
+
+
+def test_exp(benchmark):
+    benchmark(int_num, math.exp, -2.5, 2.5, 1e-4, 1000)
+
+
+def test_exp2(benchmark):
+    benchmark(int_num, math.exp, 0, -5, 1e-8, 1000)
