71f0e477e6afabee9d9a53d2b8bb3ef8c41a0014
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-10-31 09:15:35 +0100

    Allow individual groups, disable logging

--- helpers/heatmap.py
+++ helpers/heatmap.py
@@ -1,29 +1,38 @@
 import json
 import argparse
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
+from matplotlib.colors import LogNorm, Normalize
 import logging
 import sys
 import pandas as pd
 import seaborn as sns
 import os
+from sklearn.preprocessing import minmax_scale
 
 
 def generate_heatmap(runs: list[str], prop: str, group_by: str, color_scale: str,
-                     outpath: str):
+                     individual_groups: bool, outpath: str):
     data = pd.DataFrame()
     for run in runs:
         with open(run) as f:
             run_json = json.load(f)
         for test in run_json['benchmarks']:
             row = pd.DataFrame(
-                [{'run': os.path.split(run)[1][:4], 'group': test[group_by],
+                [{'run': os.path.split(run)[1][:4], 'test': test[group_by],
                   'value': float(test['stats'][prop])}])
             data = pd.concat([data, row], ignore_index=True)
-    data['nval'] = data.apply(lambda r: r.value / data[data.group == r.group].value.min(), axis=1)
-    data = data.pivot(index='group', columns='run', values='nval')
-    print(data)
-    sns.heatmap(data, annot=True, norm=LogNorm(), cmap=sns.color_palette('rocket_r', as_cmap=True))
+    data['value'] = data.apply(lambda r: r.value / data[data.test == r.test].value.min(), axis=1)
+    sdata = data = data.pivot(index='test', columns='run', values='value')
+    if individual_groups:
+        sdata = data.copy(deep=True)
+        sdata.loc[:, :] = minmax_scale(sdata, axis=1)
+    match color_scale:
+        case 'lin' | 'linear':
+            norm = Normalize()
+        case _:
+            norm = LogNorm()
+    sns.heatmap(sdata, annot=data, cbar=(not individual_groups), norm=norm,
+                cmap=sns.color_palette('rocket_r', as_cmap=True))
     plt.savefig(outpath, bbox_inches='tight')
     plt.close()
 
@@ -36,10 +45,10 @@ if __name__ == '__main__':
     parser.add_argument('-g', '--group-by', default='name')
     parser.add_argument('-c', '--color-scale', default='log')
     parser.add_argument('-p', '--property', default='mean')
+    parser.add_argument('-i', '--individual_groups', action='store_true')
     parser.add_argument('-o', '--output', default='heatmap.svg')
-    parser.add_argument('-v', '--verbose', action='count', default=0)
+    # parser.add_argument('-v', '--verbose', action='count', default=0)
     parser.add_argument('benchmarks', nargs='+')
     args = parser.parse_args()
-    logging.basicConfig(level=min(10, 40 - 10 * args.verbose))
     generate_heatmap(args.benchmarks, args.property, args.group_by, args.color_scale,
-                     args.output)
+                     args.individual_groups, args.output)
