a5117b597a1608a9fe79824fe6f25c4785feda31
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-11-01 22:48:58 +0100

    Implement local-adaptive Simpson's rule
    Slightly adapted from:
    https://en.wikipedia.org/wiki/Adaptive_Simpson%27s_method#Python

--- fastnumint/newton_cotes.py
+++ fastnumint/newton_cotes.py
@@ -32,3 +32,27 @@ def booles_rule(f, a, b, segcount):
     ws = chain([7], islice(cycle([32, 12, 32, 14]), segcount - 1), [7])
     area = sum(f(xi) * wi for xi, wi in zip(xs, ws)) * h * 2 / 45
     return area
+
+
+def _simpson(f, a, fa, b, fb):
+    m = (a + b) / 2
+    fm = f(m)
+    return m, fm, (fa + 4 * fm + fb) * (b - a) / 6
+
+
+def _asr(f, a, fa, m, fm, b, fb, whole, tol, maxdepth):
+    if maxdepth <= 0:
+        return whole
+    lm, flm, left = _simpson(f, a, fa, m, fm)
+    rm, frm, right = _simpson(f, m, fm, b, fb)
+    delta = (left + right - whole) / 15
+    if abs(delta) <= tol:
+        return left + right + delta
+    return _asr(f, a, fa, lm, flm, m, fm, left, tol / 2, maxdepth - 1) + \
+        _asr(f, m, fm, rm, frm, b, fb, right, tol / 2, maxdepth - 1)
+
+
+def local_adaptive_simpsons_rule(f, a, b, tol, maxdepth):
+    fa, fb = f(a), f(b)
+    m, fm, whole = _simpson(f, a, fa, b, fb)
+    return _asr(f, a, fa, m, fm, b, fb, whole, tol, maxdepth - 1)
