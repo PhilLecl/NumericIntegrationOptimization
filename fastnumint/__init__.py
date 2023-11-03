31a8852df5d690e466222ffaf4151278ee9d6265
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-11-03 11:57:33 +0100

    Change default to global_adaptive_gauss_kronrod

--- fastnumint/__init__.py
+++ fastnumint/__init__.py
@@ -1,13 +1,13 @@
 from .iter import *
 from .newton_cotes import *
 from fastnumint.gauss_quadrature import *
-from functools import lru_cache
+from functools import lru_cache, partial
 
 
 def int_num(f, a, b, tol=1e-4, maxiter=1000):
     if b < a:
         return -int_num(f, b, a, tol, maxiter)
-    return basic_iter(booles_rule)(f, a, b, tol, maxiter)
+    return global_adaptive_gauss_kronrod(f, a, b, tol, maxiter)
 
 
 def int_num_cachedf(f, a, b, tol=1e-4, maxiter=1000):
