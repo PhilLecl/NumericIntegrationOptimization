f1c34bb4c6bb52db80f6714c806b2c8cb225eb6c
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-11-03 11:51:00 +0100

    Remove duplicate cache

--- fastnumint/__init__.py
+++ fastnumint/__init__.py
@@ -7,7 +7,7 @@ from functools import lru_cache
 def int_num(f, a, b, tol=1e-4, maxiter=1000):
     if b < a:
         return -int_num(f, b, a, tol, maxiter)
-    return basic_iter(booles_rule)(lru_cache(maxsize=None)(f), a, b, tol, maxiter)
+    return basic_iter(booles_rule)(f, a, b, tol, maxiter)
 
 
 def int_num_cachedf(f, a, b, tol=1e-4, maxiter=1000):
