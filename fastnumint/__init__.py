1d29b1c378d15c09e4cf1caa3498c68501501fa8
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-11-06 21:18:40 +0100

    Make non-extrapolated algorithm the default

--- fastnumint/__init__.py
+++ fastnumint/__init__.py
@@ -3,4 +3,4 @@ from fastnumint.gauss_quadrature import gauss_kronrod, gauss_legendre
 
 
 def int_num(f, a, b, tol=1e-4, maxiter=1000):
-    return gauss_kronrod.global_adaptive_extrapolation(21)(f, a, b, tol, maxiter)
+    return gauss_kronrod.global_adaptive(21)(f, a, b, tol, maxiter)
