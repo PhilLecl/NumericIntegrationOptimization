266b5cd7b29ad63373a5cf861bdb924639bb612b
Philipp Leclercq <PhilLecl@users.noreply.github.com>
2023-11-09 15:57:27 +0100

    Fix test_original

--- tests/test_int_num.py
+++ tests/test_int_num.py
@@ -7,12 +7,12 @@ from .functions import *
 from .de_doncker import TEST_CASES as DE_DONCKER_TESTS
 
 MY_TESTS = (('f', 'a', 'b', 'tol', 'maxiter'), (
+    (psi_harm, -5, 5, 1e-8, 1000),
     (psi_harm, 5, -5, 1e-8, 1000),
     (psi_harm_sq, -1, 1, 1e-8, 1000),
     (psi_harm_sq, -2, 2, 1e-8, 1000),
     (psi_harm_sq, -5, 5, 1e-4, 1000),
     (psi_harm_sq, -5, 5, 1e-6, 1000),
-    (psi_harm_sq, -5, 5, 1e-8, 1000),
     (psi_harm_sq, -5, 5, 1e-10, 1000),
     (psi_harm_sq, -10, 10, 1e-8, 1000),
     (math.exp, 0, -10, 1e-8, 1000),
@@ -49,7 +49,7 @@ def format_param(val):
 
 def test_original(benchmark):
     """The test case originially supplied in the class"""
-    f, a, b, tol, maxiter = psi_harm, -5, 5, 1e-8, 1000
+    f, a, b, tol, maxiter = psi_harm_sq, -5, 5, 1e-8, 1000
     my_result = benchmark(int_num, f, a, b, tol, maxiter)
     scipy_result = integrate.quad(f, a, b)
     assert abs(my_result - scipy_result[0]) <= tol
